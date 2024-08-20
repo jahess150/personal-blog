94e903f0c7147c766d17aabe2b5b33b6bd20b1a4
Josh Hess <jahess150@gmail.com>
2024-08-20 19:46:25 -0400

    Server is functioning

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Post = require('./models/post');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Post = require("./models/post");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const app = express();
 
 // Include routing for blog posts
-const postRoutes = require('./routes/posts');
-app.use('/posts', postRoutes);
+const postRoutes = require("./routes/posts");
+app.use("/posts", postRoutes);
 
 // Connect to MongoDB
 
-require('dotenv').config(); // Import environment variables
+require("dotenv").config(); // Import environment variables
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
 // Set up middleware for route handling
-app.use(bodyParser.urlencoded({ extended: true  }));
-app.use(methodOverride('_method')); // Allow HTTP overrides for REST API
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); // Allow HTTP overrides for REST API
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 // Placeholder route for testing server
-app.get('/', (req, res) => {
-    res.send('Hello, personal blog!');
+app.get("/", (req, res) => {
+    res.send("Hello, personal blog!");
 });
 
 // Start the server
--- routes/posts.js
+++ routes/posts.js
@@ -1,25 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Post = require('../models/post');
+const Post = require("../models/post");
 
 // INDEX - Show all blog posts
-router.get('/', async (res, req) => {
+router.get("/", async (req, res) => {
     try {
         const posts = await Post.find({});
-        res.render('index', { posts });
+        res.render("index", { posts });
     } catch (err) {
         console.log(err);
-        res.redirect('/');
+        res.redirect("/");
     }
 });
 
 // NEW - Show form to create a new post
-router.get('/new', (req, res) => {
-    res.render('new');
+router.get("/new", (req, res) => {
+    res.render("new");
 });
 
 // CREATE - Add a new post to MongoDB database
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
     const newPost = {
         title: req.body.title,
         content: req.body.content,
@@ -27,37 +27,37 @@ router.post('/', async (req, res) => {
 
     try {
         await Post.create(newPost);
-        res.redirect('/posts');
+        res.redirect("/posts");
     } catch (err) {
         console.log(err);
-        res.redirect('/posts/new');
+        res.redirect("/posts/new");
     }
 });
 
 // SHOW - Show details of a single post
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        res.render('show', { post });
+        res.render("show", { post });
     } catch (err) {
         console.log(err);
-        res.redirect('/posts');
+        res.redirect("/posts");
     }
 });
 
 // EDIT - Show form to edit a post
-router.get('/:id/edit', async (req, res) => {
+router.get("/:id/edit", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        res.render('edit', { post });
+        res.render("edit", { post });
     } catch (err) {
         console.log(err);
-        res.redirect('/posts')
+        res.redirect("/posts");
     }
 });
 
 // UPDATE - Update a specific post
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
     const updatedPost = {
         title: req.body.title,
         content: req.body.content,
@@ -68,18 +68,18 @@ router.put('/:id', async (req, res) => {
         res.redirect(`/posts/${req.params.id}`);
     } catch (err) {
         console.log(err);
-        res.redirect('/posts');
+        res.redirect("/posts");
     }
 });
 
 // DELETE - Delete a specific post
-router.delete('/:id', async (req, res) => {
+router.delete("/:id", async (req, res) => {
     try {
         await Post.findByIdAndRemove(req.params.id);
-        res.redirect('/posts');
+        res.redirect("/posts");
     } catch (err) {
         console.log(err);
-        res.redirect('/posts');
+        res.redirect("/posts");
     }
 });
 
